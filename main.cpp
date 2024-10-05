68aeb6cfffe602656c86fd66438ee17da5f43669
Abe <abenanduri1@gmail.com>
2024-10-04 23:47:15 -0700

    second commit

--- main.cpp
+++ main.cpp
@@ -1,8 +1,27 @@
 //COMSC 210 | Lab 19 | Abhishek Nanduri
 
 #include<iostream>
+#include<vector>
 
 using namespace std; 
 
+class Movie 
+{
+    private: 
+        string title;
+        struct Node 
+        {
+            double data; 
+            string review; 
+        }; 
+
+    public: 
+        Movie() {}
 
 
+};
+
+int main() {
+
+
+}
